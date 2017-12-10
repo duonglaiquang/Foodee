8350accc363e593a732f9abf6299d4e4e9e8f3dc
duonglq <duonglaiquang@gmail.com>
2017-12-10 14:19:41 +0700

    abc

--- resources/views/sites/product.blade.php
+++ resources/views/sites/product.blade.php
@@ -136,136 +136,48 @@
                 </div>
             </div>
         </div>
-        {{--<div class="row" id="comment_1">--}}
         <div class="row">
-            <div class="col-sm-3">
-                <div class="rating-block">
-                    <h4>Average user rating</h4>
-                    <h2 class="bold padding-bottom-7">4.3
-                        <small>/ 5</small>
-                    </h2>
-                    <button type="button" class="btn btn-warning btn-sm" aria-label="Left Align">
-                        <span class="glyphicon glyphicon-star " style="font-size:13px;" aria-hidden="true"></span>
-                    </button>
-                    <button type="button" class="btn btn-warning btn-sm" aria-label="Left Align">
-                        <span class="glyphicon glyphicon-star" style="font-size:13px;" aria-hidden="true"></span>
-                    </button>
-                    <button type="button" class="btn btn-warning btn-sm" aria-label="Left Align">
-                        <span class="glyphicon glyphicon-star" style="font-size:13px;" aria-hidden="true"></span>
-                    </button>
-                    <button type="button" class="btn btn-default btn-grey btn-sm" aria-label="Left Align">
-                        <span class="glyphicon glyphicon-star" style="font-size:13px;" aria-hidden="true"></span>
-                    </button>
-                    <button type="button" class="btn btn-default btn-grey btn-sm" aria-label="Left Align">
-                        <span class="glyphicon glyphicon-star" style="font-size:13px;" aria-hidden="true"></span>
-                    </button>
-                </div>
-            </div>
-            <div class="col-sm-3">
-                <h4>Rating breakdown</h4>
-                <div class="pull-left">
-                    <div class="pull-left" style="width:35px; line-height:1;">
-                        <div style="height:9px; margin:5px 0;">5 <span class="glyphicon glyphicon-star"
-                                                                       style="font-size:13px;"></span></div>
-                    </div>
-                    <div class="pull-left" style="width:180px;">
-                        <div class="progress" style="height:9px; margin:8px 0;">
-                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="5"
-                                 aria-valuemin="0" aria-valuemax="5" style="width: 1000%">
-                                <span class="sr-only">80% Complete (danger)</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pull-right" style="margin-left:10px;">1</div>
-                </div>
-                <div class="pull-left">
-                    <div class="pull-left" style="width:35px; line-height:1;">
-                        <div style="height:9px; margin:5px 0;">4 <span class="glyphicon glyphicon-star"
-                                                                       style="font-size:13px;"></span></div>
-                    </div>
-                    <div class="pull-left" style="width:180px;">
-                        <div class="progress" style="height:9px; margin:8px 0;">
-                            <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="4"
-                                 aria-valuemin="0" aria-valuemax="5" style="width: 80%">
-                                <span class="sr-only">80% Complete (danger)</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pull-right" style="margin-left:10px;">1</div>
-                </div>
-                <div class="pull-left">
-                    <div class="pull-left" style="width:35px; line-height:1;">
-                        <div style="height:9px; margin:5px 0;">3 <span class="glyphicon glyphicon-star"
-                                                                       style="font-size:13px;"></span></div>
-                    </div>
-                    <div class="pull-left" style="width:180px;">
-                        <div class="progress" style="height:9px; margin:8px 0;">
-                            <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="3"
-                                 aria-valuemin="0" aria-valuemax="5" style="width: 60%">
-                                <span class="sr-only">80% Complete (danger)</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pull-right" style="margin-left:10px;">0</div>
-                </div>
-                <div class="pull-left">
-                    <div class="pull-left" style="width:35px; line-height:1;">
-                        <div style="height:9px; margin:5px 0;">2 <span class="glyphicon glyphicon-star"
-                                                                       style="font-size:13px;"></span></div>
-                    </div>
-                    <div class="pull-left" style="width:180px;">
-                        <div class="progress" style="height:9px; margin:8px 0;">
-                            <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="2"
-                                 aria-valuemin="0" aria-valuemax="5" style="width: 40%">
-                                <span class="sr-only">80% Complete (danger)</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pull-right" style="margin-left:10px;">0</div>
-                </div>
-                <div class="pull-left">
-                    <div class="pull-left" style="width:35px; line-height:1;">
-                        <div style="height:9px; margin:5px 0;">1 <span class="glyphicon glyphicon-star"
-                                                                       style="font-size:13px;"></span></div>
-                    </div>
-                    <div class="pull-left" style="width:180px;">
-                        <div class="progress" style="height:9px; margin:8px 0;">
-                            <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="1"
-                                 aria-valuemin="0" aria-valuemax="5" style="width: 20%">
-                                <span class="sr-only">80% Complete (danger)</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pull-right" style="margin-left:10px;">0</div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div id="comment_1">
-                @include("sites.comment");
-            </div>
+
             @if(Auth::check())
                 <form id="postComment" action="{{route('addComment')}}" method="get">
-                    <!-- Form input fields here (do not forget your name attributes). -->
-                    <div>
+                    <input type="hidden" id="product_1" name="product_1" value="{{$prd_detail->id}}">
+                    <div class="comment-box">
+                        <div class="well well-sm" style="background: whitesmoke !important;">
+                            <div class="commentlb">What do you think abour our product ?</div>
+                            <div class="text-right">
+                                <a style="font-size: 1em" class="btn btn-success btn-green" href="#reviews-anchor"
+                                   id="open-review-box">Leave a
+                                    Review</a>
+                            </div>
 
-                        <ul class="list-group" style="margin-right: 502px !important;">
+                            <div class="row" id="post-review-box" style="display:none;">
+                                <div class="col-md-12">
+                                    <form accept-charset="UTF-8" action="" method="post">
+                                        <input id="ratings-hidden" name="rating" type="hidden">
+                                        <textarea class="form-control animated" cols="50" id="comment_text"
+                                                  name="comment_text"
+                                                  placeholder="Enter your review here..." rows="3" style="resize: none"></textarea>
 
-                            <li class="list-group-item">
-                                {{--<i id="styleId" name="styleId" value="{{$showStyle->id}}"></i>--}}
-                                <input type="hidden" id="product_1" name="product_1" value="{{$prd_detail->id}}">
+                                        <div class="text-right">
+                                            <div class="stars starrr" data-rating="0"></div>
+                                            <a class="btn btn-danger btn-sm" href="#" id="close-review-box"
+                                               style="display:none; margin-right: 10px;">
+                                                <span class="glyphicon glyphicon-remove"></span>Cancel</a>
+                                            <button class="btn btn-success btn-lg" type="submit">Save</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
 
-                                <input type="text" style="width: 100%; padding: 10px;" id="comment_text" name="comment_text"
-                                       placeholder="Add comment..."/>
-                            </li>
-                            <button type="submit" class="comment-button">addComment</button>
-                        </ul>
                     </div>
                 </form>
             @endif
 
         </div>
-
+        <div id="comment_1">
+            @include("sites.comment");
+        </div>
         <div class="row">
             <div class="product-other">
                 <div class="row">
